bcc3167f6ef6b9438846e02d709209c19ea16edc
Fluffy_J <222191562@mycput.ac.za>
2025-08-05 18:36:23 +0200

    index view complete

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Title, Body
+
+admin.site.site_header = "Notes Admin"
+admin.site.site_title = "Notes Admin Area"
+admin.site.index_title = "Welcome to the Notes Admin"
+
+class BodyTextInline(admin.TabularInline):
+    model = Body
+    extra = 0
+
+class TitleAdmin(admin.ModelAdmin):
+     fieldsets = [(None, {'fields': ['title_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+     inlines = [BodyTextInline]
+
+admin.site.register(Title, TitleAdmin)
+
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
-# Create your views here.
+
+from .models import Title
+
+def index(request):
+    latest_title_list = Title.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("notes/index.html")
+    context = {"latest_title_list": latest_title_list}
+    return render(request, "notes/index.html", context)
+
+
+def detail(request, title_id):
+    title = get_object_or_404(Title, pk=title_id)
+    return render(request, "notes/detail.html", {"title": title})
