d46211d05cd939faee6ed9f52e0708b224e89937
Fluffy_J <222191562@mycput.ac.za>
2025-08-05 12:35:08 +0200

    model complete

--- notes/models.py
+++ notes/models.py
@@ -4,10 +4,18 @@ class Title(models.Model):
     title_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Date published")
 
+    def __str__(self):
+        return self.title_text
+
 
 class Body(models.Model):
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
     body_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.body_text
+
+
 
 
 
