c63835ca0af5f77c70f7b3bb7e45a55ed5934ae3
Fluffy_J <222191562@mycput.ac.za>
2025-08-06 11:18:20 +0200

    made changes to the schema changed relationship to OneToOne

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'notes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:title_id>', views.detail, name = 'detail')
+    path('<int:title_id>', views.detail, name='detail')
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
 
-from .models import Title
+from .models import Title, Body
 
 def index(request):
     latest_title_list = Title.objects.order_by("-pub_date")[:5]
@@ -13,4 +13,5 @@ def index(request):
 
 def detail(request, title_id):
     title = get_object_or_404(Title, pk=title_id)
-    return render(request, "notes/detail.html", {"title": title})
+    body = title.body_set.get(pk=title.id)
+    return render(request, "notes/detail.html", {"title": title, "body": body})
