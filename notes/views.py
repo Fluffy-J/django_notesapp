f88b9826efa70be69b611e1da780efeef4345b91
Fluffy_J <222191562@mycput.ac.za>
2025-08-07 17:16:24 +0200

    view note complete and creat note complete

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
-
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
 from .models import Title, Body
 
@@ -15,3 +16,20 @@ def detail(request, title_id):
     title = get_object_or_404(Title, pk=title_id)
     body = title.body
     return render(request, "notes/detail.html", {"title": title, "body": body})
+
+
+def make_note(request):
+    return render(request, "pages/makenote.html")
+
+
+def create(request):
+    if request.method == 'POST':
+        title_text = request.POST.get('title')
+        body_text = request.POST.get('body_text')
+
+        title = Title.objects.create(title_text = title_text, pub_date = timezone.now())
+        Body.objects.create(title=title, body_text=body_text)
+
+        return redirect('notes:make_note')
+
+    return render(request, 'pages/makenote.html')
