c8057457b78872223f00f61c659646ebeea6db31
Fluffy_J <222191562@mycput.ac.za>
2025-09-16 19:36:32 +0200

    category model not intergrating well into title model

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
 class Title(models.Model):
     title_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title_text
@@ -15,9 +18,3 @@ class Body(models.Model):
 
     def __str__(self):
         return self.body_text
-
-
-
-
-
-
