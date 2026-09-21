3be0c6389b824b54f9e3ba8b72ff24b801efdf82
Fluffy-J <222191562@mycput.ac.za>
2026-09-21 07:40:40 +0200

    model_tets_complete

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Title, Body
+
+class NoteModelTests(TestCase):
+    def setUp(self):
+        self.title = Title.objects.create(
+            title_text="Test Note"
+        )
+        
+        self.body = Body.objects.create(
+            title=self.title,
+            body_text="This is the body of the test note."
+        )
+        
+    def test_title_is_created(self):
+        self.assertEqual(self.title.title_text, "Test Note")
+        
+    def test_title_string_representation(self):
+        self.assertEqual(str(self.title), "Test Note")
+        
+    def test_body_is_linked_to_title(self):
+        self.assertEqual(self.body.title, self.title)
+        self.assertEqual(self.title.body, self.body)
+    
+    def test_deleting_title_deletes_body(self):
+        self.title.delete()
+        self.assertEqual(Body.objects.count(),0)
+        
+     
